71eecc5cfbea46524324b17bb106adad65d7f8f7
alesharik <alesharik4@gmail.com>
2025-11-08 13:37:50 +0300

    fix: does not compile

--- crates/teloxide/src/dispatching/dispatcher.rs
+++ crates/teloxide/src/dispatching/dispatcher.rs
@@ -415,12 +415,6 @@ where
         let stop_token = Some(update_listener.stop_token());
         self.start_listening(update_listener, update_listener_error_handler, stop_token).await;
 
-        self.start_listening(
-            update_listener,
-            update_listener_error_handler,
-            stop_token,
-        ).await;
-
         Ok(())
     }
 
